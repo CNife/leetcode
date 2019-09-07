bc8b0e1fb4e721b1c9e8fe060fa35879503a43b5
CNife <CNife@vip.qq.com>
2019-09-07 18:56:23 +0800

    Reconstruct Original Digits from English 0ms 3.1MB

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,117 @@
-pub struct Solution;
-
 impl Solution {
-    pub fn find_longest_word(s: String, d: Vec<String>) -> String {
-        let mut result = "".to_string();
-        for word in d {
-            if Solution::in_word(&s, &word) {
-                if word.len() > result.len() || word.len() == result.len() && word < result {
-                    result = word;
-                }
+    pub fn original_digits(s: String) -> String {
+        let mut map = CharMap::from_str(&s);
+        let mut result = vec![];
+
+        loop {
+            let digit = map.next_digit();
+            if digit > 9 {
+                break;
+            } else {
+                map.minus(digit);
+                result.push(DIGITS[digit]);
             }
         }
-        result
-    }
 
-    fn in_word(src: &str, word: &str) -> bool {
-        let src = src.as_bytes();
-        let word = word.as_bytes();
-        let mut src_idx = 0usize;
-        let mut word_idx = 0usize;
-
-        while src_idx < src.len() && word_idx < word.len() {
-            if word[word_idx] == src[src_idx] {
-                word_idx += 1;
-            }
-            src_idx += 1;
-        }
-        word_idx == word.len()
+        result.sort_unstable();
+        result.into_iter().collect::<String>()
     }
 }
 
-#[test]
-pub fn test_find_longest_word() {
-    let cases = vec![
-        ("abpcplea", vec!["ale", "apple", "monkey", "plea"], "apple"),
-        ("abpcplea", vec!["a", "b", "c"], "a"),
-    ];
+struct CharMap([u32; 26]);
 
-    for (string, dict, expected) in cases {
-        let string = string.to_string();
-        let dict: Vec<_> = dict.into_iter().map(|s| s.to_string()).collect();
-        let result = Solution::find_longest_word(string, dict);
-        assert_eq!(result, expected.to_string());
+impl CharMap {
+    fn from_str(s: &str) -> CharMap {
+        let mut map = [0u32; 26];
+        for c in s.as_bytes() {
+            let idx = *c as u8 - 'a' as u8;
+            map[idx as usize] += 1;
+        }
+        CharMap(map)
+    }
+
+    #[inline]
+    fn is_empty(&self) -> bool {
+        self.0 == [0; 26]
+    }
+
+    fn next_digit(&self) -> usize {
+        if self.is_empty() {
+            return 10;
+        }
+        for &(idx, digit) in &DIGIT_EXISTS_MAP {
+            if self.0[idx] > 0 {
+                return digit;
+            }
+        }
+        unreachable!();
+    }
+
+    fn minus(&mut self, digit: usize) {
+        for i in 0usize..26 {
+            self.0[i] -= DIGITS_MAP[digit as usize][i];
+        }
+    }
+}
+
+const DIGITS: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+const DIGIT_EXISTS_MAP: [(usize, usize); 10] = [
+    (25, 0),
+    (22, 2),
+    (23, 6),
+    (20, 4),
+    (17, 3),
+    (18, 7),
+    (14, 1),
+    (21, 5),
+    (19, 8),
+    (8, 9),
+];
+
+const DIGITS_MAP: [[u32; 26]; 10] = [
+    [
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+    ],
+    [
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+    ],
+    [
+        0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ],
+];
+
+pub struct Solution;
+
+#[test]
+fn test_original_digits() {
+    let cases = vec![
+        ("owoztneoer", "012"),
+        ("fviefuro", "45"),
+        ("zerozero", "00"),
+    ];
+    for (s, expected) in cases {
+        assert_eq!(Solution::original_digits(s.to_string()), expected);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
-macro_rules! print_size_of_type {
-    ($type: ty) => {
-        println!(
-            "sizeof({}) = {}",
-            stringify!($type),
-            std::mem::size_of::<$type>()
-        );
-    };
-}
-
 fn main() {
-    print_size_of_type!(&mut Vec<i32>);
-    print_size_of_type!(Option<&mut Vec<i32>>);
+    let nums = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    print!("      ");
+    for ch in 0u8..26 {
+        let ch = ch + 'a' as u8;
+        print!("{:^3}", ch as char);
+    }
+    println!();
+
+    for &num in &nums {
+        let mut counts = [0; 26];
+        for ch in num.as_bytes() {
+            let index = ch - 'a' as u8;
+            counts[index as usize] += 1;
+        }
+        // println!("{:?}", counts);
+
+        print!("{:6}", num);
+        for &count in &counts {
+            if count == 0 {
+                print!("   ");
+            } else {
+                print!("{:^3}", count);
+            }
+        }
+        println!();
+    }
 }
