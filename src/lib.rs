0ed780973ff9e422a65d5115b0727a080e50aac6
CNife <CNife@vip.qq.com>
2019-09-10 15:05:24 +0800

    201 Bitwise AND of Numbers Range 0ms 2.4MB

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,20 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
-        let mut rows = vec![];
-        let mut columns = vec![];
-
-        for (r, row) in matrix.iter().enumerate() {
-            for (c, elem) in row.into_iter().enumerate() {
-                if *elem == 0 {
-                    rows.push(r);
-                    columns.push(c);
-                }
+    pub fn range_bitwise_and(m: i32, n: i32) -> i32 {
+        for i in 0..32 {
+            if (m >> i) == (n >> i) {
+                return m & (-1 << i);
             }
         }
-
-        for r in rows {
-            for elem in matrix[r].iter_mut() {
-                *elem = 0;
-            }
-        }
-
-        for c in columns {
-            for r in 0..matrix.len() {
-                matrix[r][c] = 0;
-            }
-        }
+        unreachable!();
     }
 }
 
 #[test]
-fn test_set_zeroes() {
-    let cases = vec![
-        (
-            vec![vec![1, 1, 1], vec![1, 0, 1], vec![1, 1, 1]],
-            vec![vec![1, 0, 1], vec![0, 0, 0], vec![1, 0, 1]],
-        ),
-        (
-            vec![vec![0, 1, 2, 0], vec![3, 4, 5, 2], vec![1, 3, 1, 5]],
-            vec![vec![0, 0, 0, 0], vec![0, 4, 5, 0], vec![0, 3, 1, 0]],
-        ),
-    ];
-    for (mut matrix, expected) in cases {
-        Solution::set_zeroes(&mut matrix);
-        assert_eq!(matrix, expected);
+fn test_range_bitwise_and() {
+    let cases = vec![(5, 7, 4), (0, 1, 0)];
+    for (m, n, expected) in cases {
+        assert_eq!(Solution::range_bitwise_and(m, n), expected);
     }
 }
