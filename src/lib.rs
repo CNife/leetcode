28afabeb6ee50fe5d3577d174cb94f5b6af04700
CNife <CNife@vip.qq.com>
2019-09-20 19:04:34 +0800

    59. 螺旋矩阵 II 0ms 2.1MB

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,55 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn next_greater_element(n: i32) -> i32 {
-        let mut digits = n.to_string().into_bytes();
-        let (i, ei) = match digits
-            .iter()
-            .enumerate()
-            .zip(digits[1..].iter())
-            .rfind(|((_, el), er)| el < er)
-        {
-            Some((res, _)) => res,
-            None => return -1,
+    pub fn generate_matrix(n: i32) -> Vec<Vec<i32>> {
+        let n = n as usize;
+        let mut matrix = Solution::init_matrix(n);
+
+        let mut counter = 0i32;
+        let mut next = || {
+            counter += 1;
+            counter
         };
-        let (j, _) = digits.iter().enumerate().rfind(|&(_, ej)| ej > ei).unwrap();
-        digits.swap(i, j);
-        digits[i + 1..].reverse();
-        unsafe {
-            String::from_utf8_unchecked(digits)
-                .parse::<i32>()
-                .unwrap_or(-1)
+
+        let mut i = 0;
+        let mut j = n - 1;
+        while i < j {
+            for c in i..j {
+                matrix[i][c] = next();
+            }
+            for r in i..j {
+                matrix[r][j] = next();
+            }
+            for c in (i + 1..j + 1).rev() {
+                matrix[j][c] = next();
+            }
+            for r in (i + 1..j + 1).rev() {
+                matrix[r][i] = next();
+            }
+            i += 1;
+            j -= 1;
         }
+        if i == j {
+            matrix[i][i] = next();
+        }
+        matrix
+    }
+
+    fn init_matrix(n: usize) -> Vec<Vec<i32>> {
+        let mut matrix = Vec::with_capacity(n);
+        for _ in 0..n {
+            let mut row = Vec::with_capacity(n);
+            row.resize(n, 0);
+            matrix.push(row);
+        }
+        matrix
     }
 }
 
 #[test]
-fn test_next_greater_element() {
-    let cases = vec![(12, 21), (21, -1)];
+fn test_generate_matrix() {
+    let cases = vec![(3, vec![vec![1, 2, 3], vec![8, 9, 4], vec![7, 6, 5]])];
     for (n, expected) in cases {
-        let output = Solution::next_greater_element(n);
-        assert_eq!(output, expected);
+        assert_eq!(Solution::generate_matrix(n), expected);
     }
 }
