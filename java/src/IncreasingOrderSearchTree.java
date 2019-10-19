b787d2d317d1504b901b4a1cbfac705d18b5a5ab
CNife <CNife@vip.qq.com>
2019-10-19 16:27:02 +0800

    897. 递增顺序查找树 E Java 5ms 41.9MB

---
+++ java/src/IncreasingOrderSearchTree.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class IncreasingOrderSearchTree {
+    private static ArrayList<Integer> toList(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        return list;
+    }
+
+    private static void inOrder(TreeNode node, ArrayList<Integer> list) {
+        if (node.left != null) {
+            inOrder(node.left, list);
+        }
+        list.add(node.val);
+        if (node.right != null) {
+            inOrder(node.right, list);
+        }
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        if (root == null)
+            return null;
+
+        ArrayList<Integer> values = toList(root);
+        TreeNode newRoot = new TreeNode(values.get(0));
+        TreeNode ptr = newRoot;
+        for (int i = 1; i < values.size(); i++)
+            ptr = ptr.right = new TreeNode(values.get(i));
+        return newRoot;
+    }
+}
