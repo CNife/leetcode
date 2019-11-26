af7717d20f2ff5bb0a296372e1e8aa168442e4e8
CNife <CNife@vip.qq.com>
2019-11-26 11:53:01 +0800

    399. 除法求值 M Rust 0ms 2.1MB

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,22 @@
-pub mod edit_distance;
+pub mod evaluate_division;
+
+pub mod utils {
+    use std::collections::HashSet;
+    use std::hash::Hash;
+    use std::iter::FromIterator;
+
+    pub fn sv(sv: Vec<&str>) -> Vec<String> {
+        sv.into_iter().map(|s| s.to_string()).collect()
+    }
+
+    pub fn svv(svv: Vec<Vec<&str>>) -> Vec<Vec<String>> {
+        svv.into_iter().map(sv).collect()
+    }
+
+    pub fn s<T>(v: Vec<T>) -> HashSet<T>
+    where
+        T: Eq + Hash,
+    {
+        HashSet::from_iter(v.into_iter())
+    }
+}
