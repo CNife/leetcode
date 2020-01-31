45a104cf646ec58fcdc67e2d22e9f0ae8f9a7ea4
CNife <CNife@vip.qq.com>
2020-01-31 19:44:56 +0800

    LCP 3. 机器人大冒险 M Java 1753ms 37.2MB

---
+++ java/src/ProgrammableRobot.java
@@ -0,0 +1,114 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProgrammableRobot {
+    public static boolean robot(String commands, int[][] obstacles, int x, int y) {
+        Game game = new Game(obstacles, x, y);
+        char[] commandArray = commands.toCharArray();
+
+        while (true) {
+            for (char command : commandArray) {
+                game.moveRobot(command);
+                if (game.failed()) {
+                    return false;
+                } else if (game.succeeded()) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    @Test
+    void test() {
+        TestCase[] cases = new TestCase[]{
+                new TestCase("URR", new int[][]{}, 3, 2, true),
+                new TestCase("URR", new int[][]{new int[]{2, 2}}, 3, 2, false),
+                new TestCase("URR", new int[][]{new int[]{4, 2}}, 3, 2, true)
+        };
+        for (TestCase testCase : cases) {
+            boolean output = robot(testCase.command, testCase.obstacles, testCase.x, testCase.y);
+            assertEquals(testCase.expected, output);
+        }
+    }
+
+    private static class Game {
+        Point robot;
+        Set<Point> obstacles;
+        Point destination;
+
+        Game(int[][] obstacles, int x, int y) {
+            this.robot = new Point(0, 0);
+            this.destination = new Point(x, y);
+            this.obstacles = Arrays.stream(obstacles)
+                    .map(obstacle -> new Point(obstacle[0], obstacle[1]))
+                    .collect(Collectors.toSet());
+        }
+
+        boolean failed() {
+            return obstacles.contains(robot) || robot.x > destination.x || robot.y > destination.y;
+        }
+
+        boolean succeeded() {
+            return robot.equals(destination);
+        }
+
+        void moveRobot(char command) {
+            switch (command) {
+                case 'U':
+                    robot.y++;
+                    break;
+                case 'R':
+                    robot.x++;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return x == point.x &&
+                    y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    private static class TestCase {
+        String command;
+        int[][] obstacles;
+        int x;
+        int y;
+        boolean expected;
+
+        TestCase(String command, int[][] obstacles, int x, int y, boolean expected) {
+            this.command = command;
+            this.obstacles = obstacles;
+            this.x = x;
+            this.y = y;
+            this.expected = expected;
+        }
+    }
+}
