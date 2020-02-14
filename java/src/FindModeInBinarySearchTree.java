5c140efb18ae8d6d7fabb384e18c99b532d9d18d
CNife <CNife@vip.qq.com>
2020-02-14 14:13:48 +0800

    501. 二叉搜索树中的众数 E Java 18ms 49.7MB

---
+++ java/src/FindModeInBinarySearchTree.java
@@ -0,0 +1,57 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FindModeInBinarySearchTree {
+    private static int[] findMode(TreeNode root) {
+        if (root == null) {
+            return new int[]{};
+        }
+
+        List<TreeNode> stack = new ArrayList<>();
+        TreeNode top = root;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while (!stack.isEmpty() || top != null) {
+            while (top != null) {
+                stack.add(top);
+                top = top.left;
+            }
+            if (!stack.isEmpty()) {
+                top = stack.get(stack.size() - 1);
+                map.put(top.val, map.getOrDefault(top.val, 0) + 1);
+                stack.remove(stack.size() - 1);
+                top = top.right;
+            }
+        }
+
+        int maxCount = 0;
+        List<Integer> result = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (maxCount < entry.getValue()) {
+                result.clear();
+                result.add(entry.getKey());
+                maxCount = entry.getValue();
+            } else if (maxCount == entry.getValue()) {
+                result.add(entry.getKey());
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }
+
+    @Test
+    void test() {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.right = new TreeNode(2);
+        int[] expected = new int[]{2};
+
+        int[] result = findMode(root);
+        Assertions.assertArrayEquals(expected, result);
+    }
+}
