aca0306452951d489dfd688b38e72dc8457e4d9e
CNife <CNife@vip.qq.com>
2020-07-01 16:29:15 +0800

    面试题 10.01. 合并排序的数组 E Python

--- Python/leetcode/test.py
+++ Python/leetcode/test.py
@@ -1,10 +1,12 @@
 import sys
 
 
-def test(function, test_cases, map_func=None, eq_func=None):
+def test(function, test_cases, get_func=None, map_func=None, eq_func=None):
     for test_case in test_cases:
         args, expect = test_case[:-1], test_case[-1]
         actual = function(*args)
+        if get_func:
+            actual = get_func(test_case)
         if map_func:
             actual, expect = map_func(actual), map_func(expect)
         if (eq_func and not eq_func(actual, expect)) or actual != expect:
