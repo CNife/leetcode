22686790a34a5c0b9ff72fad2a60375df00192e1
CNife <CNife@vip.qq.com>
2021-01-03 10:14:10 +0800

    86. 分隔链表 M Golang 重写

--- go/before_commit.ps1
+++ go/before_commit.ps1
@@ -3,4 +3,15 @@ $ErrorActionPreference = "Stop"
 $tests = Get-ChildItem -Directory -Exclude '.*' | ForEach-Object { $_.FullName }
 
 go test $tests
+if ($LASTEXITCODE -ne 0)
+{
+    throw 'testing'
+}
+
 go fmt $tests
+if ($LASTEXITCODE -ne 0)
+{
+    throw 'formatting'
+}
+
+Write-Output 'Success!'
