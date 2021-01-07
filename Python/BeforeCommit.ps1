80c5c32b847b01b9f150e87187616a17d80e19e1
CNife <CNife@vip.qq.com>
2021-01-07 19:09:24 +0800

    547. 省份数量 M Golang

--- Python/BeforeCommit.ps1
+++ Python/BeforeCommit.ps1
@@ -7,6 +7,12 @@ $env:PYTHONPATH = $PSScriptRoot
 
 Get-ChildItem $PSScriptRoot\code\*.py | ForEach-Object -Parallel {
     python $_.FullName
+    if ($LASTEXITCODE -ne 0) {
+        throw "$_.FullName failed"
+    }
 }
 
 python -m black $PSScriptRoot\code $PSScriptRoot\leetcode
+if ($LASTEXITCODE -ne 0) {
+    throw "format failed"
+}
