9e38e21af7e408dc87d4abe1edf5db15a05d767b
CNife <CNife@vip.qq.com>
2021-01-10 10:37:23 +0800

    228. 汇总区间 E Golang

--- go/before_commit.ps1
+++ go/before_commit.ps1
@@ -2,12 +2,14 @@ $ErrorActionPreference = "Stop"
 
 $tests = Get-ChildItem -Directory -Exclude '.*' | ForEach-Object { $_.FullName }
 
+Write-Output 'Testing'
 go test $tests
 if ($LASTEXITCODE -ne 0)
 {
     throw 'testing'
 }
 
+Write-Output 'Formatting'
 go fmt $tests
 if ($LASTEXITCODE -ne 0)
 {
